1fc903657ba115c03980294aa7f417cea3b40c99
Facundo <facurael@gmail.com>
2024-07-10 19:20:05 -0300

    feat:add feature/CheckIn

--- justina-io/src/components/atoms/ActionButton.jsx
+++ justina-io/src/components/atoms/ActionButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ActionButton = ({ text, className }) => {
   return (
-    <button className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${className}`}>
+    <button className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-purple-600 disabled:pointer-events-none disabled:opacity-50 ${className}`}>
       {text}
     </button>
   );
---
+++ justina-io/src/layouts/LayoutsNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar';
+
+const LayoutsNav = ({children}) => {
+  return (
+    <>
+        <Navbar />
+        {children}
+    </>
+  )
+}
+
+export default LayoutsNav;
