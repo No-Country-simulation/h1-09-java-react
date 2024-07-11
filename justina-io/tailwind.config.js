ef271ec4b07eb7e2c604e42cfd1ac5e5f27df530
Alan Rios <riosalan264@gmail.com>
2024-07-11 00:50:40 -0300

    feat: add sidebar in home

--- justina-io/tailwind.config.js
+++ justina-io/tailwind.config.js
@@ -1,6 +1,6 @@
 import daisyui from "daisyui";
-/** @type {import('tailwindcss').Config} */
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -31,4 +31,3 @@ export default {
     themes: ["light"],
   },
 };
-
