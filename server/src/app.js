e0c47f639e7699f65b782d2fc7bcb52ed5c34bea
Juampi095 <azarjpm15@gmail.com>
2024-07-18 11:29:39 -0300

    se establecio el secreto y la variable jwt_secret

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
-import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config();
 
 const app = express();
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors("*"));
 app.use(express.json());
 
-app.get("/", (req, res) => { res.json({ status: 'success' }) });
+app.get("/", (req, res) => {
+  res.json({ status: "success" });
+});
 
 export default app;
