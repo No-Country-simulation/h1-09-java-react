d4a7fb3849e13b97dbea9a8ca68fa024ba68f257
Juampi095 <azarjpm15@gmail.com>
2024-07-23 16:07:07 -0300

    Se agrego la ruta de citas a App.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 
 import authRoute from "./router/auth.routes.js";
+import citasRouter from "./router/citas.routes.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors("*"));
 app.use(express.json());
 
 app.use("/api", authRoute);
+app.use("/api", citasRouter);
 
 app.get("/", (req, res) => {
   res.json({ status: "success" });
