ca05176a135f2388adb12e3877881c9cf9c8ce49
Adrian <esteban.adrian.gonz@gmail.com>
2024-07-25 09:00:04 -0300

    ref: integrate association with PersonalMedico & add new fields to User

--- server/src/services/users.services.js
+++ server/src/services/users.services.js
@@ -1,7 +1,8 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import PersonalMedico from "../models/PersonalMedico.js";
 
-export async function getUserByEmail() {
+export async function getUserByEmail(email) {
   return User.findOne({
     where: {
       email: email,
@@ -9,10 +10,21 @@ export async function getUserByEmail() {
   });
 }
 
-export async function createUser() {
-  const hashedPassword = await bcrypt.hash(User.password, 10);
-  return User.create({
-    email: User.email,
+export async function createUser({ email, password, nombre, apellido, telefono, estado_civil, especialidad, nro_matricula }) {console.log(email, password)
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = User.create({
+    email,
     password: hashedPassword,
-  });
+    nombre,
+    apellido,
+    telefono,
+    estado_civil: estado_civil.toLowerCase(),
+    especialidad,
+    nro_matricula
+  })
+  PersonalMedico.create({
+    idPersonalMedico: await user?.id
+  })
+  
+  return user;
 }
