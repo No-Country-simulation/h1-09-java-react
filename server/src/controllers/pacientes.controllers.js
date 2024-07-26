60a01d06c01acafcdff0cad91961761af7d5eed1
Juampi095 <azarjpm15@gmail.com>
2024-07-26 11:01:08 -0300

    Se agrego return en pacientes.controllers y citas.controllers

--- server/src/controllers/pacientes.controllers.js
+++ server/src/controllers/pacientes.controllers.js
@@ -3,9 +3,9 @@ import Paciente from "../models/Paciente.js";
 export const obtenerPacientes = async (req, res) => {
   try {
     const pacientes = await Paciente.findAll();
-    res.json(pacientes);
+    return res.json(pacientes);
   } catch (err) {
-    res.status(500).json({ err: "Error al obtener los pacientes " });
+    return res.status(500).json({ err: "Error al obtener los pacientes " });
   }
 };
 
@@ -13,11 +13,11 @@ export const obtenerPacientesById = async (req, res) => {
   try {
     const paciente = await Paciente.findByPk(req.params.id);
     if (paciente) {
-      res.json(paciente);
+      return res.json(paciente);
     } else {
-      res.status(404).json({ err: "Paciente no encontrado" });
+      return res.status(404).json({ err: "Paciente no encontrado" });
     }
   } catch (err) {
-    res.status(500).json({ err: "Error al obtener al paciente" });
+    return res.status(500).json({ err: "Error al obtener al paciente" });
   }
 };
