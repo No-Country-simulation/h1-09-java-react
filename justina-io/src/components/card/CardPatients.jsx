ed7704d493dffd0c6cdfa75934858cd70edd8f30
Facundo <facurael@gmail.com>
2024-07-27 14:57:48 -0300

    feat: add new feature/connect-fetch-patients

--- justina-io/src/components/card/CardPatients.jsx
+++ justina-io/src/components/card/CardPatients.jsx
@@ -1,28 +1,59 @@
-import React from 'react';
-import Card from '../card/Card'; 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from '../card/Card';
 
 const CardPatients = () => {
-  const patients = [
-    { name: 'Mateo Fernandez', photo: 'img/image6.png' },
-    { name: 'Lucas García', photo: 'img/frame5.png' },
-    { name: 'Sofía Rodríguez', photo: 'img/image1.png' },
-    { name: 'Mateo Fernandez', photo: 'img/image3.png' },
-    { name: 'Lucas García', photo: 'img/frame29.png' },
-    { name: 'Sofía Rodríguez', photo: 'img/image1.png' },
-    { name: 'Mateo Fernandez', photo: 'img/image6.png' },
-    { name: 'Lucas García', photo: 'img/frame5.png' },
-    { name: 'Sofía Rodríguez', photo: 'img/image6.png' },
+  const [patients, setPatients] = useState([]);
+  const [error, setError] = useState(null);
+  const photos = [
+    'img/image6.png',
+    'img/frame5.png',
+    'img/image1.png',
+    'img/image3.png',
+    'img/frame29.png',
+    'img/image1.png',
+    'img/image6.png',
+    'img/frame5.png',
+    'img/image6.png',
   ];
 
+  useEffect(() => {
+   
+    const fetchPatients = async () => {
+      try {
+        const response = await axios.get('https://h1-09-java-react.onrender.com/api/pacientes');
+        console.log('API Response:', response.data);
+        const patientsData = response.data.map((patient, index) => ({
+          name: `${patient.nombre} ${patient.apellido}`,
+          photo: photos[index % photos.length],
+        }));
+        setPatients(patientsData);
+      } catch (error) {
+        console.error('Error fetching patients data:', error);
+        setError('Error fetching patients data.');
+      }
+    };
+
+    fetchPatients();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <div className="h-screem mx-auto py-4">
-      {patients.map((patient, index) => (
-        <Card
-          key={index}
-          photo={patient.photo}
-          name={patient.name}
-        />
-      ))}
+    <div className="h-screen mx-auto py-4">
+      {patients.length === 0 ? (
+        <p>No patients found</p>
+      ) : (
+        patients.map((patient, index) => (
+          <Card
+            key={index}
+            photo={patient.photo}
+            name={patient.name}
+          />
+        ))
+      )}
     </div>
   );
 };
