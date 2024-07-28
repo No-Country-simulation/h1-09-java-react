2b918a2924d6e6101cb6ee2f4ac9da5215067400
Adrian <esteban.adrian.gonz@gmail.com>
2024-07-28 20:50:45 -0300

    feat: add missing fields

--- server/src/models/GenericUser.js
+++ server/src/models/GenericUser.js
@@ -20,6 +20,46 @@ const GenericUser = sequelize.define("genericUser", {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  edad: {
+    type: DataTypes.INTEGER,
+  },
+  peso: {
+    type: DataTypes.INTEGER,
+  },
+  grupo_sanguineo: {
+    type: DataTypes.STRING,
+  },
+  genero: {
+    type: DataTypes.STRING,
+  },
+  altura: {
+    type: DataTypes.STRING,
+  },
+  alergias: {
+    type: DataTypes.STRING,
+  },
+  telefono: {
+    type: DataTypes.INTEGER,
+  },
+  direccion: {
+    type: DataTypes.STRING,
+  },
+  contacto_familiar_telefono: {
+    type: DataTypes.INTEGER,
+  },
+  contacto_familiar_relacion: {
+    type: DataTypes.STRING,
+  },
+  profile_picture: {
+    type: DataTypes.STRING
+  },
+  idPrepaga: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "prepagas",
+      key: "idPrepagas"
+    }
+  }
 });
 
 export default GenericUser;
